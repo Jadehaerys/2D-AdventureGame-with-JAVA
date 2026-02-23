6886f301a49fc7eda85dfcf05548f10a80b9e2a5
unknown <jmykelvenz@gmail.com>
2026-02-23 22:01:28 +0800

    added boots

--- object/OBJ_Boots.java
+++ object/OBJ_Boots.java
@@ -1,5 +1,28 @@
 package object;
 
-public class OBJ_Boots {
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class OBJ_Boots extends SuperObject {
+
+     public OBJ_Boots() {
+        name = "Boots";
+        try {
+            InputStream is = getClass().getResourceAsStream("/objects/boots.png");
+            if (is != null) {
+                image = javax.imageio.ImageIO.read(is);
+            } else {
+                File f = new File("resources/objects/boots.png");
+                if (f.exists()) {
+                    image = javax.imageio.ImageIO.read(f);
+                } else {
+                    System.err.println("Missing resource: /objects/boots.png");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        collision = true;
+    }
 }
