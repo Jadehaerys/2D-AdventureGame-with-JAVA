e531a89dbefc67d14a009e59680164189c7fa180
unknown <jmykelvenz@gmail.com>
2026-03-01 18:53:37 +0800

    Added sound

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -55,6 +55,9 @@ public class GamePanel extends JPanel implements Runnable {
 
    public void setupGame() {
        aSetter.setObject();
+
+        playMusic(0);
+       
    }
    
    public void startGameThread() {
