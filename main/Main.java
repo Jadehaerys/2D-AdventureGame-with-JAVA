ab4036c52bf811d63d9018679c13311b8beb928a
Jadehaerys <jmykelvenz@gmail.com>
2026-07-23 02:24:02 +0800

    test

--- main/Main.java
+++ main/Main.java
@@ -18,5 +18,4 @@ public class Main {
       gamePanel.setupGame();
       gamePanel.startGameThread();
    }
-
 }
--- main/Sound.java
+++ main/Sound.java
@@ -3,30 +3,37 @@ package main;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.AudioInputStream;
+import java.io.File;
 import java.net.URL;
 
 public class Sound {
 
     Clip clip;
-    URL soundURL[] = new URL[30];
-
-    public Sound() {
-        soundURL[0] = getClass().getResource("/resources/sound/JadeAdventure.wav");
-        soundURL[1] = getClass().getResource("/resources/sound/coin.wav");
-        soundURL[2] = getClass().getResource("/resources/sound/powerup.wav");
-        soundURL[3] = getClass().getResource("/resources/sound/unlock.wav");
-        soundURL[4] = getClass().getResource("/resources/sound/fanfare.wav");
-        soundURL[5] = getClass().getResource("/resources/sound/hitmonster.wav");
-    }
+    String soundFile[] = {
+        "JadeAdventure.wav",
+        "coin.wav",
+        "powerup.wav",
+        "unlock.wav",
+        "fanfare.wav",
+        "hitmonster.wav"
+    };
 
     public void setFile(int i) {
         try {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            AudioInputStream ais = getAudioInputStream(soundFile[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private AudioInputStream getAudioInputStream(String fileName) throws Exception {
+        URL url = getClass().getResource("/sound/" + fileName);
+        if (url != null) {
+            return AudioSystem.getAudioInputStream(url);
+        }
+        return AudioSystem.getAudioInputStream(new File("resources/sound/" + fileName));
     }
     public void play() {
         clip.start();
